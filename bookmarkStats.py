a092adb2953eda19a84785228fc7c36238ab9c85
doyler <dev@doyler.net>
2016-02-24 12:48:21 +0100

    Minor updates, ie11 -> ie

--- bookmarkStats.py
+++ bookmarkStats.py
@@ -9,7 +9,7 @@ def createSoup(browser):
     soup = BeautifulSoup(html, 'html.parser')
     return soup
 
-def headerList(browser, theSoup, linkList):
+def printHeaderList(browser, theSoup, linkList):
     headerList = theSoup.findAll('h3')
     
     if browser == "chrome" or browser == "firefox":
@@ -19,13 +19,13 @@ def headerList(browser, theSoup, linkList):
         header = ''.join(node.findAll(text=True))
         s = node
         while getattr(s, 'name', None) != 'dl':
-            if browser == "chrome" or browser == "ie11":
+            if browser == "chrome" or browser == "ie":
                 s = s.nextSibling
             elif browser == "firefox":
                 s = s.findNext('dl')
         if browser == "chrome" or browser == "firefox":
             parents = len(node.findParents('dl')) - 2
-        elif browser == "ie11":
+        elif browser == "ie":
             parents = len(node.findParents('dl')) - 1
         count = len(s.findAll('a'))
         percentage = "{0:.2f}%".format(((count + 0.0)/len(linkList)) * 100)
@@ -41,18 +41,22 @@ def populateList(linkList, urlType):
         elif urlType == "noProtocol":
             noProtocol = link['href'].split('://', 1)[-1]
             urlList.append(str(noProtocol))
+        """
+        # Removing this for now as there doesn't seem to be a use case
         elif urlType == "noQueryString":
             questionSplit = link['href'].rsplit('?', 1)
             if len(questionSplit) > 1:
                 noQueryString = questionSplit[0]
                 urlList.append(str(noQueryString))
+        """
     return urlList
 
 def getDupes(inList):        
     return [item for item, c in collections.Counter(inList).items() if c > 1]
         
 def main():
-    browser = "ie11"
+    supportedBrowsers = ["chrome", "firefox", "ie"]
+    browser = "chrome"
     
     mySoup = createSoup(browser)
     linkList = mySoup.find_all('a')
@@ -65,31 +69,32 @@ def main():
     total = len(linkList)
     print "Total number of bookmarks: " + str(total) + "\n"
     
-    headerList(browser, mySoup, linkList)
+    if any(browser in s for s in supportedBrowsers):
+        printHeaderList(browser, mySoup, linkList)
 
-    urlList = populateList(linkList, "normal")    
-    dupes = getDupes(urlList)
-    print "\n\nDUPLICATE LINKS = " + str(len(dupes))
-    print "----------------"
-    for dupe in dupes:
-        print dupe
+        urlList = populateList(linkList, "normal")    
+        dupes = getDupes(urlList)
+        print "\n\nDUPLICATE LINKS = " + str(len(dupes))
+        print "----------------"
+        for dupe in dupes:
+            print dupe
         
-    urlList = populateList(linkList, "noProtocol")    
-    dupes = getDupes(urlList)    
-    print "\nDUPLICATE LINKS (IGNORING PROTOCOL) = " + str(len(dupes))
-    print "------------------------------------"
-    for dupe in dupes:
-        print dupe
+        urlList = populateList(linkList, "noProtocol")    
+        dupes = getDupes(urlList)    
+        print "\nDUPLICATE LINKS (IGNORING PROTOCOL) = " + str(len(dupes))
+        print "------------------------------------"
+        for dupe in dupes:
+            print dupe
 
-    """
-    # Maybe remove? This isn't super useful, and hasn't caught any actual dupes    
-    urlList = populateList(linkList, "noQueryString")    
-    dupes = getDupes(urlList)
-    print "\nDUPLICATE LINKS (IGNORING QUERYSTRING) = " + str(len(dupes))
-    print "---------------------------------------"
-    for dupe in dupes:
-        print dupe
-    """
+        """
+        # Removing this for now as there doesn't seem to be a use case
+        urlList = populateList(linkList, "noQueryString")    
+        dupes = getDupes(urlList)
+        print "\nDUPLICATE LINKS (IGNORING QUERYSTRING) = " + str(len(dupes))
+        print "---------------------------------------"
+        for dupe in dupes:
+            print dupe
+        """
         
 if __name__=="__main__":
 	main()
